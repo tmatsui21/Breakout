3076b2b0cd981f3043d44a27775c9221a0111a59
tmatsui21 <tmatsui21@gmail.com>
2012-10-01 18:53:22 +0900

    minor updates

--- breakout.js
+++ breakout.js
@@ -1,16 +1,16 @@
 /*
  * a very simple Breakout game
  *     for Javascript OOP study
- * @version 0.1
+ * @version 0.2
  * @author Takashi Matsui
  * Ball extends Paddle and adds Move method
  * Brick extends Paddle and overrides Clear method
+ * https://github.com/tmatsui21/Breakout
  */
 
 var BLOCK = 20, WIDTH = 300, HIGHT = 400;
 var DELTAX = 5, DELTAY = -5;
 var timer = null;
-
 var canvas = document.getElementById('canvas');
 
 // class definitions
@@ -21,30 +21,27 @@ function Paddle(x, y, ctx) {
     this.w = BLOCK * 5;
     this.h = BLOCK;
 }
-
 Paddle.prototype.draw = function (color) {
     this.ctx.fillStyle = color;
     this.ctx.fillRect(this.x, this.y, this.w, this.h);
 }
-
 Paddle.prototype.clear = function () {
     this.ctx.clearRect(this.x, this.y, this.w, this.h);
 }
+Paddle.prototype.intersect = function (other){
+    return this.x <= other.x + other.w && this.x + this.w >= other.x &&
+        this.y <= other.y + other.h && this.y + this.h >= other.y;	
+};
 
 function Ball() {
     Paddle.apply(this, arguments); // プロパティの継承
     this.w = BLOCK; // プロパティの変更
 }
-
 Ball.prototype = new Paddle(); // メソッドの継承
-
 Ball.prototype.move = function () { // メソッドの追加
-    if ((this.x + BLOCK) >= WIDTH || this.x < 0) DELTAX *= -1;
-    if ((this.y + BLOCK) >= HIGHT || this.y < 0) DELTAY *= -1;
-    if (intersect(this, paddle)) DELTAY *= -1;
-    if (clearBlock()) DELTAY *= -1;
-    
     if ((this.y + BLOCK) > 370) stop();
+    if ((this.x + BLOCK) >= WIDTH || this.x < 0) DELTAX *= -1;
+    if ((this.y + BLOCK) >= HIGHT || this.y < 0 || this.intersect(paddle) || clearBlock()) DELTAY *= -1;
     this.clear();
     this.x += DELTAX;
     this.y += DELTAY;
@@ -55,7 +52,6 @@ function Brick() {
     Paddle.apply(this, arguments);
     this.w = BLOCK * 3;
 }
-
 Brick.prototype = new Paddle();
 Brick.prototype.clear = function () { // メソッドのオーバーライド
     this.ctx.clearRect(this.x, this.y, this.w, this.h);
@@ -103,16 +99,9 @@ function stop() {
     }
 }
 
-function intersect(A,B){
-    if ((A.x <= B.x + B.w) && (A.x +A.w >= B.x) && (A.y <= B.y + B.h) && (A.y + A.h >= B.y)){
-        return true;
-    }
-    return false;		
-};
-
 function clearBlock() {
     for (var i = 0; i < 9; i++) {
-        if (intersect(ball, bricks[i])) {
+        if (ball.intersect(bricks[i])) {
             bricks[i].clear();
             return true;
         }
